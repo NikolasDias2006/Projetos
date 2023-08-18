8168cf8948a2f41bcba8c0e7738bca12a005f365
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-17 22:01:49 -0300

    javascript

--- Projetos-com-ajuda/Pokedex/js/script.js
+++ Projetos-com-ajuda/Pokedex/js/script.js
@@ -1,1 +1,31 @@
-/*Parei na parte 30:08*/
+pokemoname = document.querySelector(".pokemon_name")
+pokemonumber = document.querySelector(".pokemon_number")
+pokemonimagem = document.querySelector(".pokemon_imagem")
+
+ /* async sicroniza com await para que o programa so seja executado ser onde está o await retornar um valor. */
+
+  /*funcao*/
+const fetchpokemon = async (pokemon) => {
+    /*pegar os dados*/
+    const Apiresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    );
+    /*Geralmente os que estao com await sao dados que demora para carregar,e entao é nercessario para carregar de uma vez*/
+
+    const data = await Apiresponse.json(); /* Carregar todos os dados / extraindo os dados */
+
+    return data;
+}
+
+const renderpokemon = async (pokemon) => {
+    const data = await fetchpokemon(pokemon);  
+    
+    pokemoname.innerHTML = data.name;
+    pokemonumber.innerHTML = data.id;
+
+    pokemonimagem.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    
+}
+
+renderpokemon('3')
+
+/*Parei na parte 42:30*/
