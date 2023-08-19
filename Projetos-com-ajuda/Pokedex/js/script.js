c8ebe6bfa8b595c2b38aa889e09aa3a840ffc3dd
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-19 18:37:37 -0300

    search fucionando

--- Projetos-com-ajuda/Pokedex/js/script.js
+++ Projetos-com-ajuda/Pokedex/js/script.js
@@ -1,6 +1,8 @@
-pokemoname = document.querySelector(".pokemon_name")
-pokemonumber = document.querySelector(".pokemon_number")
-pokemonimagem = document.querySelector(".pokemon_imagem")
+const pokemoname = document.querySelector(".pokemon_name")
+const pokemonumber = document.querySelector(".pokemon_number")
+const pokemonimagem = document.querySelector(".pokemon_imagem")
+const pokemonform = document.querySelector(".form")
+const pokemonsearch = document.querySelector(".input_search")
 
  /* async sicroniza com await para que o programa so seja executado ser onde está o await retornar um valor. */
 
@@ -25,7 +27,15 @@ const renderpokemon = async (pokemon) => {
     pokemonimagem.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
     
 }
+/*Quando enviar o formulario,irá executar o seguinte comando: */
 
-renderpokemon('3')
+pokemonform.addEventListener("submit",function (evento) {
+    /* o valor que irá digitar */
+    search = pokemonsearch.value..toLowerCase()
+    /*para impedir a execuçao inicial dele,ou seja ao apertar para pesquisar nao enviar na hora ate que der enter*/
+
+    evento.preventDefault();
+    renderpokemon(search)
+})
 
 /*Parei na parte 42:30*/
