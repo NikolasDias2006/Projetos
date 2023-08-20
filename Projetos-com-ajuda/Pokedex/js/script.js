5a431fbafeb77aaa9c997a798cf4449b2566e5ef
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-20 19:50:27 -0300

    prev e next

--- Projetos-com-ajuda/Pokedex/js/script.js
+++ Projetos-com-ajuda/Pokedex/js/script.js
@@ -4,27 +4,37 @@ const pokemonimagem = document.querySelector(".pokemon_imagem")
 const pokemonform = document.querySelector(".form")
 const pokemonsearch = document.querySelector(".input_search")
 
+const prev = document.querySelector('.btn-prev')
+const next = document.querySelector('.btn-next')
+
+let searchpokemon = 150
+
  /* async sicroniza com await para que o programa so seja executado ser onde está o await retornar um valor. */
 
   /*funcao*/
 const fetchpokemon = async (pokemon) => {
+    
+    pokemoname.innerHTML = 'loading.....';
+
     /*pegar os dados*/
     const Apiresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`
     );
-    
+
+    pokemoname.innerHTML = '';
+    pokemonumber.innerHTML = '';
+
     if(Apiresponse.status == 200) {
     /*Geralmente os que estao com await sao dados que demora para carregar,e entao é nercessario para carregar de uma vez*/
 
     const data = await Apiresponse.json(); /* Carregar todos os dados / extraindo os dados */
-
+    searchpokemon = data.id
     return data;
     } else {
         pokemonimagem.src = 'https://i.pinimg.com/originals/f6/8c/0e/f68c0eacc02886c44f1c5be8f3098f0c.png'
 
-        pokemoname.style.color = "black";
         pokemonumber.style.color = "black";
-        pokemoname.innerHTML = '!';
-        pokemonumber.innerHTML = 'Not Found';
+        pokemoname.innerHTML = '';
+        pokemonumber.innerHTML = 'Not Found !';
         
     }
 }
@@ -50,6 +60,23 @@ pokemonform.addEventListener("submit",function (evento) {
 
     evento.preventDefault();
     renderpokemon(search)
+
 })
 
-/*Parei na parte 50:32*/
+/* prev button */
+prev.addEventListener("click",function(prev) {
+  /* o minimo para da prev é ate 1, menor que isso nao irá fucionar */
+    if(searchpokemon > 1) {
+    searchpokemon -= 1
+    renderpokemon(searchpokemon)
+   }
+})
+
+/* next buttom */
+next.addEventListener("click",function(next) {
+  searchpokemon +=1
+  renderpokemon(searchpokemon)
+})
+
+/* para aparecer na tela inicial */
+renderpokemon(searchpokemon)
