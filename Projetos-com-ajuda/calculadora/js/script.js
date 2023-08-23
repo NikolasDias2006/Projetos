db791076e081e5d5318a3e8f06b5fdd4eccbe307
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-22 22:01:52 -0300

    javascript

---
+++ Projetos-com-ajuda/calculadora/js/script.js
@@ -0,0 +1,15 @@
+/* PreviousOperationText */
+const preoutex =  document.querySelector("#previous-operation")
+/*  CurrentOPerationText */
+const cuoptext = document.querySelector("#current-opration")
+
+/* há varios botoes por isso queryselectorALL */
+const buttons = document.querySelectorAll("#buttons-container button")
+
+class Calculator {
+    
+}
+
+buttons.foreach()
+
+/* parei no minuto 21:40 */
