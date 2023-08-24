b98256a775edcefec49e897c9232097eb4699f98
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-23 21:57:09 -0300

    craindo uma class e constructor

--- Projetos-com-ajuda/calculadora/js/script.js
+++ Projetos-com-ajuda/calculadora/js/script.js
@@ -1,15 +1,48 @@
 /* PreviousOperationText */
 const preoutex =  document.querySelector("#previous-operation")
 /*  CurrentOPerationText */
-const cuoptext = document.querySelector("#current-opration")
-
+const cuoptext = document.querySelector("#current-operation")
 /* há varios botoes por isso queryselectorALL */
 const buttons = document.querySelectorAll("#buttons-container button")
 
 class Calculator {
-    
+    /* o constructor add items que sera obrigatorio colocar ao usa as variaveis que está no constructor sem precisar poluir o codigo*/
+     constructor(preoutex,cuoptext) {
+        this.preoutex = preoutex
+        this.cuoptext = cuoptext
+        this.currentoperation = ""
+     }
+
+     //adicionando digito para a tela da calculadora
+    adddigit(digit) {
+        this.currentoperation = digit
+        this.uptadescreen()
+ }
+
+ //trocando/atualizando valores da tela da calculadora
+
+    uptadescreen() {
+        this.cuoptext.innerText += this.currentoperation
+ }
 }
 
-buttons.foreach()
 
-/* parei no minuto 21:40 */
+   // add novos elementos...
+   const calc = new Calculator(preoutex,cuoptext)
+
+
+   /* Adicionando os eventos */
+   buttons.forEach((btn) => {
+        btn.addEventListener("click",(e) => { 
+         const value = e.target.innerText
+
+         /* + para converter para numero */
+        if(+value >=0 || value === ".") {
+           calc.adddigit(value)
+        } else {
+            console.log("OP: "+value)
+        }
+        })
+})
+
+/* parei no minuto  */
