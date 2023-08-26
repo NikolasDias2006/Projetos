ce085a9201278b7c723c40e351a1c062066734db
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-25 21:59:27 -0300

    Update script.js

--- Projetos-com-ajuda/calculadora/js/script.js
+++ Projetos-com-ajuda/calculadora/js/script.js
@@ -27,14 +27,41 @@ class Calculator {
     proop = processOperation
  */
    proop(operation) {
+      //checando se o valor do curret está vazio:
+      if(cuoptext.innerText === ""){
+         //troca de operaçao
+         if(this.preoutex.innerText !== "") {
+            this.changeoperation(operation)
+         }
+         return
+      }
+
       //obtendo valores anterioes e recentes
       let operationvalue
-      const previus = +this.preoutex.innerText
+      const previus = +this.preoutex.innerText.split(" ")[0]
       const current = +this.cuoptext.innerText
 
       switch(operation) {
           case "+":
              operationvalue = previus + current
+             this.uptadescreen(operationvalue,
+              operation,current,previus)       
+         break
+
+         case "-":
+             operationvalue = previus - current
+             this.uptadescreen(operationvalue,
+              operation,current,previus)       
+         break
+
+         case "/":
+             operationvalue = previus / current
+             this.uptadescreen(operationvalue,
+              operation,current,previus)       
+         break
+
+         case "*":
+             operationvalue = previus * current
              this.uptadescreen(operationvalue,
               operation,current,previus)       
          break
@@ -50,11 +77,33 @@ class Calculator {
       ,current = null
       ,previus = null
       ) {
-        this.cuoptext.innerText += this.currentoperation
-
         console.log(operationvalue, operation, previus ,current)
+
+        if(operationvalue === null) {
+         this.cuoptext.innerText += this.currentoperation
+        } else {
+          // checando se o valor é zero,apenas adicionando value
+          if(previus === 0) {
+            operationvalue = current
+          }
+         //adicionando o valor de current para previus
+         this.preoutex.innerText =`${operationvalue} ${operation}`
+         this.cuoptext.innerText = ""
+         }
  }
-}
+
+   //troca de operadores matematicos
+    changeoperation(operation) {
+      //math Operation
+      const mathop = ["*","/","+","-"]
+      
+      if(!mathop.includes(operation)) {
+         return 
+       }
+
+      this.preoutex.innerText = this.preoutex.innerText.slice(0,-1)+operation
+   }
+  }
 
    // add novos elementos...
    const calc = new Calculator(preoutex,cuoptext)
@@ -74,4 +123,4 @@ class Calculator {
         })
 })
 
-// parei no minuto  37:34 
+// parei no minuto  
