d1394384977075631d4cb522aa28148a45809c52
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-12-02 12:01:53 -0300

    more functions

---
+++ Projetos-Treino/3DCity/script.js
@@ -0,0 +1,142 @@
+//Base Paramenters
+var renderer = new THREE.WebGLRenderer({antialias: true})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+if (window.innerWidth > 800) {
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
+    renderer.shadowMap.needsUpdate = true
+}
+
+document.body.appendChild(renderer.domElement)
+
+//Make it responsive
+window.addEventListener("resize",onWindowResize,false)
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+
+var camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 500)
+camera.position.set(0, 2, 14)
+
+//add obj 3d
+var scene = new THREE.Scene()
+var city = new THREE.Object3D()
+var smoke = new THREE.Object3D()
+var town = new THREE.Object3D()
+var createCarpos = true
+var uSpeed = 0.001
+
+// FOG background
+var setcolor = 0xF02050
+scene.background = new THREE.Color(setcolor)
+scene.fog =  new THREE.Fog(setcolor, 10, 16)
+
+//random Function 
+function mathRandom(num = 8) {
+    var numValue = - Math.random() * num + Math.random() * num
+
+    return numValue
+}
+
+//Change Building Colors
+var setTintNum = true
+ function setTintColor(){
+    if(setTintNum) {
+         setTintNum = false
+         var setcolor = 0x000000
+    } else {
+         setTintNum = true
+         var setcolor = 0x000000
+    }
+    return setcolor
+ }
+
+// Create City 
+function init() {
+    var segments = 2
+    for(var i= 1; i < 100; i++) {
+        //.CubeGeometry
+        var geometry = new THREE.BoxGeometry(1, 0, 0, segments, segments)
+        var material = new THREE.MeshStandardMaterial({
+            color: setTintColor(),
+            wireFrame: false,
+            shading: THREE.SmoothShading,
+            side: THREE.DoubleSide
+        })
+        var wmaterial = new THREE.MeshLambertMaterial({
+            color: 0xffffff,
+            wireframe: false,
+            transparent:true,
+            opacity: 0.03,
+            side: THREE.DoubleSide
+        })
+         var cube = new THREE.Mesh(geometry, material)
+         var wire = new THREE.Mesh(geometry, material)
+         var floor = new THREE.Mesh(geometry, material)
+         var wfloor = new THREE.Mesh(geometry, material)
+
+         cube.add(wfloor)
+         cube.setShadow = true
+         cube.receiveShadow = true
+         cube.rotationValue = 0.1 + Math.abs(mathRandom(8))
+         floor.scale.y = 0.05
+         cube.scale.y = 0.1 + Math.abs(mathRandom(8))
+
+         var cubeWidth = 0.9
+         cube.scale.x = cube.scale.z = cubeWidth + mathRandom(1 - cubeWidth)
+         cube.position.x = Math.round(mathRandom())
+         cube.position.z = Math.round(mathRandom())
+
+         floor.position.set(cube.position.x, 0, cube.position.z)
+
+         town.add(floor)
+         town.add(cube)
+    }
+//Particulars 
+     var gmaterial = new THREE.MeshToonMaterial({
+        color: 0xFFFF00, side: THREE.DoubleSide
+     })
+
+     var gparticular = new THREE.CircleGeometry(0.01, 3)
+     var aparticular = 5
+
+     for(var h = 1;h < 300; h++) {
+        var particular = new THREE.Mesh(gparticular, gmaterial)
+
+        particular.position.set(mathRandom(aparticular), mathRandom(aparticular),mathRandom(aparticular))
+
+        particular.rotation.set(mathRandom(), mathRandom(), mathRandom())
+
+        smoke.add(particular)
+     }
+
+      var pmaterial = new THREE.MeshPhongMaterial({
+          color: 0x000000,
+          side: THREE.DoubleSide,
+          roughness: 10,
+          metalness: 0.6,
+          opacity: 0.9,
+          transparent: true
+      })
+
+      var pgeometry = new THREE.PlaneGeometry(60, 60)
+      var pelement =  new THREE.Mesh(pgeometry,pmaterial)
+      pelement.rotation.x = -90 * Math.PI / 180
+      pelement.position.y = -0.001
+      pelement.receiveShadow = true
+      city.add(pelement)
+ }
+
+// Mouse functions
+
+
+//Calling main functions
+generateLines()
+init()
+animate()
